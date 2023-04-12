7fad9e570167bafe6a8d60d455c82148553ca0b4
m-amirazmi <m.amirazmi@hotmail.com>
2023-04-12 23:49:28 +0800

    setting text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen w-screen items-center justify-center bg-gradient-to-b from-outrageous-orange-100 to-outrageous-orange-50">
-        <div className="flex flex-col items-center justify-center gap-2 text-2xl uppercase">
+        <div className="flex flex-col items-center justify-center gap-2 text-2xl font-bold uppercase text-outrageous-orange-950">
           <p className="tracking-widest">Tepi.my</p>
           <p>Coming Soon</p>
         </div>
