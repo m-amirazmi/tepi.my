43a76317e9518195a2299f922dbd7e5f6137a3d8
Amir Azmi <amir.azmi@prodigious.com>
2023-04-13 11:32:44 +0800

    add css animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import { type NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HiBars3, HiOutlineXMark } from "react-icons/hi2";
 
 const Home: NextPage = () => {
   const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (!toggleMobileMenu && !animate) return;
+    if (toggleMobileMenu && !animate) setAnimate(true);
+    else if (!toggleMobileMenu && animate) {
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
+    }
+  }, [toggleMobileMenu]);
 
   return (
     <>
@@ -24,8 +35,8 @@ const Home: NextPage = () => {
             </div>
             <div
               className={`${
-                toggleMobileMenu ? "block" : "hidden"
-              } absolute -right-4 -top-5 h-screen w-screen bg-slate-50`}
+                toggleMobileMenu ? "open" : animate ? "animate-close" : "close"
+              } mobile-menu absolute -right-4 -top-5 h-screen w-screen bg-slate-50`}
             >
               <div className="flex px-4 py-5">
                 <div
