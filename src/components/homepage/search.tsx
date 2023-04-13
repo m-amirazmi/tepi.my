545340650d6fa3e897e79ac0ff95559a1e8e6324
Amir Azmi <amir.azmi@prodigious.com>
2023-04-13 16:53:19 +0800

    thinner bar

--- src/components/homepage/search.tsx
+++ src/components/homepage/search.tsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <section className="mx-auto max-w-6xl px-4 pt-6">
       <div className="w-full rounded bg-white shadow-sm">
-        <form className="flex items-center justify-between gap-4 p-4">
+        <form className="flex items-center justify-between gap-4 px-4 py-2">
           <div className="md:5/6 flex w-4/5 flex-row items-center gap-3">
             <div className="text-2xl text-zinc-500">
               <HiMagnifyingGlass />
@@ -16,7 +16,7 @@ const Search = () => {
             />
           </div>
           <div className="w-1/5 md:w-1/6">
-            <button className="w-full rounded bg-outrageous-orange-900 p-2 text-outrageous-orange-50">
+            <button className="w-full rounded bg-outrageous-orange-900 px-2 py-1 text-outrageous-orange-50">
               Find
               <span className="hidden md:inline"> Stall</span>
             </button>
