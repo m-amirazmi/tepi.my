6b663a3c6e9224352c2a22fcd0218aefd05b4f44
Amir Azmi <amir.azmi@prodigious.com>
2023-04-13 14:15:40 +0800

    make the header fixed

--- src/components/general/header.tsx
+++ src/components/general/header.tsx
@@ -4,7 +4,7 @@ import MainMenu from "./main-menu";
 
 const Header = () => {
   return (
-    <header className="w-full">
+    <header className="fixed w-full bg-white text-zinc-700 shadow">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-5">
         <Link href="/" className="text-xl font-semibold tracking-wider">
           <span>TEPI</span>
---
+++ src/components/homepage/announcement.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { HiArrowLongRight } from "react-icons/hi2";
+
+const Announcement = () => {
+  return (
+    <section className="w-full">
+      <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-6">
+        <div className="flex w-full flex-col items-center justify-between gap-4 rounded bg-outrageous-orange-200/20 px-6 py-4 md:flex-row">
+          <p>
+            Join us and share any road side stalls you have visited here! Let
+            others have the same experience with yours.
+          </p>
+          <Link
+            className="ml-auto flex w-full items-center justify-start gap-2 text-outrageous-orange-900 hover:text-outrageous-orange-900/70 md:w-auto"
+            href="/"
+          >
+            <span>Share stalls</span>
+            <span className="text-xl">
+              <HiArrowLongRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Announcement;
